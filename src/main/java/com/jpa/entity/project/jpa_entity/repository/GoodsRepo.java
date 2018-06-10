640f3260dfd721def83851214eb1edca5cf2ceff
eves <evesjapan@gmail.com>
2018-06-10 14:58:56 +0900

    comment add

--- src/main/java/com/jpa/entity/project/jpa_entity/repository/GoodsRepo.java
+++ src/main/java/com/jpa/entity/project/jpa_entity/repository/GoodsRepo.java
@@ -4,5 +4,8 @@ import com.jpa.entity.project.jpa_entity.models.GoodsEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
+/**
+ * 수정포인트.
+ */
 public interface GoodsRepo extends JpaRepository<GoodsEntity, Long>, QuerydslPredicateExecutor<GoodsEntity> {
 }
